b5aa14553580931526502c6282f1596b392a2640
C. Reid <ctlnrd@gmail.com>
2017-03-03 23:42:59 -0500

    wordChoices - works!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wins = 0;
+var wins = document.getElementById("wins");
 var guessesRemaining = 6;
 // var currentWord = 
 
@@ -9,31 +9,73 @@ document.onkeyup = function () {
 	console.log(userGuess);
 
 	var wordChoices = [
-		["grown woman"],
-	 	// 	["holdup"],
-	 	// 	["blue ft blue ivy"],
-	 	// 	["flawless"],
-	  	// 	["all night"]
+		["g", "r", "o", "w", "n", "w", "o", "m", "a", "n"],
+ 		["h", "o", "l", "d", "u", "p"],
+ 		["b", "l", "u", "e", "f", "t", "b", "l", "u", "e", "i", "v", "y"],
+ 		["f", "l", "a", "w", "l", "e", "s", "s"],
+  		["a", "l", "l", "n", "i", "g", "h", "t"]
 	]
-	var computerChoice = wordChoices[Math.floor(Math.random()*wordChoices.length)];
-
-	console.log(computerChoice)
-
-	function myFunction {
-		var str = computerChoice;
-		var res = str.replace(" _ ")
-	}
-
 	
-	console.log(currentWord);
+	var random = wordChoices[Math.floor(Math.random()*wordChoices.length)];
+	// var random = Math.floor((Math.random()*(wordChoices.length-1))); 
+	console.log(random); // chooses one of the words in the array
 
-	if ((computerChoice == "grown woman") && (userGuess === "G" || userGuess === "R" || userGuess === "O" || 
-		userGuess === "W" || userGuess === "N" || userGuess === "M" || userGuess === "A"   )) {
-		console.log("userGuess");	
+	// // every letter in the word is symbolized by an underscore in the guessfield
+	for (var i = 0; i < random.length; i++){
+		
+		console.log(random.length);
+		random[i] = "_ ";	
 	}
+
+	// // function printCurrentWord(){
+	function printCurrentWord(){
+		
+
+
+		for (var i = 0; i < random.length; i++){
+			var field = document.getElementById("field"); //create new variable called 'field'
+			var letter = document.createTextNode(random[i]); // create new variable called 'letter' 
+			field.appendChild(letter);
+		}
+	}
+
+	printCurrentWord();
 }
+
+
+
+
+
+
+	// print the Current Word to the console
+	// function printCurrentWord(){
+	// 	for (var i = 0; i < ratewort.length; i++){
+	// 		var currentWord = document.getElementById("currentWord");
+	// 		var letters = document.createTextNode(currentWord[i]);
+	// 	underscore.appendChild(buchstabe);
+	// }
+	// }
+
+	// if ((computerChoice == "grown woman") && (userGuess === "G" || userGuess === "R" || userGuess === "O" || 
+	// 	userGuess === "W" || userGuess === "N" || userGuess === "M" || userGuess === "A"   )) {
+	// 	console.log("userGuess");	
+	// }
+
+	// var foo = document.getElementById("wins")
+	// foo.innerHTML = 'Wins: ' + wins;
+
+	// console.log(currentWord);
+
+	// Take user guess, and loop through array and see if it matches any letter in the word
+	
+	//str.split(""), str.join("")
+
+	// Iterate over string/array, if user choice matches letter, replace _ with letter on page
+
 
 // GET ELEMENT BY ID 
 
 
 // var currentWord = wordChoices[random]; 
+
+
